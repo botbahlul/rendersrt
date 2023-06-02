1a5dcf038b7579fc67742061083dea5fe261e391
Bot Bahlul <88623122+botbahlul@users.noreply.github.com>
2023-06-03 07:09:12 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 from __future__ import unicode_literals
 import os
 import sys
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning, module='setuptools')
+warnings.filterwarnings("ignore", category=UserWarning, module='setuptools')
+warnings.filterwarnings("ignore", message=".*is deprecated*")
 
 try:
     from setuptools import setup
